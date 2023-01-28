0e7aaa9d978dd8af4d006987f774e91018d384f7
Danila Kurach <danyakurach@gmail.com>
2023-01-28 14:11:58 +0300

    21 & 121 in python

---
+++ 121. Best Time to Buy and Sell Stock/python/solve.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        left = 0
+        right = 1
+        max_profit = 0
+
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit, max_profit)
+            else:
+                left = right
+            right += 1
+
+        return max_profit
+
