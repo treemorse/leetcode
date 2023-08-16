381254a6426479bc7d32766c025a1b160151113b
Danila Kurach <danyakurach@gmail.com>
2023-08-16 12:49:48 +0300

    im very dumb

--- problems/167. Two Sum II - Input Array Is Sorted/golang/main.go
+++ problems/167. Two Sum II - Input Array Is Sorted/golang/main.go
@@ -2,19 +2,42 @@ package main
 
 import "fmt"
 
+// bad solution
+//func twoSum(numbers []int, target int) []int {
+//	result := []int{}
+//	for i := 0; i <= len(numbers); i++ {
+//		for j := 1; j <= len(numbers)-1; j++ {
+//			if j != i {
+//				if numbers[i]+numbers[j] == target {
+//					result = append(result, i+1, j+1)
+//					return result
+//				}
+//			}
+//		}
+//	}
+//	return result
+//}
+
 func twoSum(numbers []int, target int) []int {
-	result := []int{}
-	for i := 0; i <= len(numbers); i++ {
-		for j := 1; j <= len(numbers)-1; j++ {
-			if j != i {
-				if numbers[i]+numbers[j] == target {
-					result = append(result, i+1, j+1)
-					return result
-				}
-			}
+	l, r := 0, len(numbers)-1
+
+	for {
+		sum := numbers[l] + numbers[r]
+
+		if sum > target {
+			r--
+			continue
+		}
+
+		if sum < target {
+			l++
+			continue
+		}
+
+		if sum == target {
+			return []int{l + 1, r + 1}
 		}
 	}
-	return result
 }
 
 func main() {
