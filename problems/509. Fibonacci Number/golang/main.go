1672a429c0e22eaa9fadcf2f961de2c88c3356fd
Daniel Zondberg <dzondberg@gmail.com>
2023-08-18 22:46:07 +0300

    Update main.go
    memoization

--- problems/509. Fibonacci Number/golang/main.go
+++ problems/509. Fibonacci Number/golang/main.go
@@ -1,9 +1,11 @@
 package golang
 
-func fib(n int) int {
-	if n <= 1 {
-		return n
-	}
+func fib(n int, cache []int) int {
+    if cache[n] != -1 {
+        return cache[n]
+    }
 
-	return fib(n-1) + fib(n-2)
+    cache[n] = fib(n-1, cache) + fib(n-2, cache)
+    return cache[n]
 }
+
